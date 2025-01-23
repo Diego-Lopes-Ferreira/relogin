e1095833476ef8629ea348aa6d29639b9c7727be
Diego-Lopes-Ferreira <dglpferreira@gmail.com>
2025-01-23 11:41:05 -0300

    feat: Hide time
      New button to add time
      Refactor: floating div instead of floating button

--- code.js
+++ code.js
@@ -1,8 +1,9 @@
 const mainTextTime = document.querySelector("#time");
-const pomodoro = new PomodoroHandler("pomodoro", 25);
-const pause = new PomodoroHandler("pause", 5);
-const stopwatch = new StopWatchHandler("stopwatch");
-const custom = new CustomHandler("custom-handler");
+const btnShowHideSideTime = document.querySelector("#showhidetime");
+btnShowHideSideTime.onclick = () => {
+  mainTextTime.classList.toggle("hide");
+  mainTextTime.classList.toggle("show");
+};
 
 const sidebar = document.querySelector("aside");
 const btnShowHideSidebar = document.querySelector("#showhideui");
@@ -11,13 +12,18 @@ btnShowHideSidebar.onclick = () => {
   sidebar.classList.toggle("show");
 };
 
+const pomodoro = new PomodoroHandler("pomodoro", 25);
+const pause = new PomodoroHandler("pause", 5);
+const stopwatch = new StopWatchHandler("stopwatch");
+const custom = new CustomHandler("custom-handler");
+
 function update() {
   pomodoro.update();
   pause.update();
   stopwatch.update();
   custom.update();
   mainTextTime.textContent = generateTimeStrByDate();
-  navigator.getBattery().then(battery => 
+  navigator.getBattery().then(battery =>
     document.querySelector("#battery").textContent = battery.level*100 + "%"
   );
   setTimeout(update, 1000);
